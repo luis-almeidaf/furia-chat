db75a292136c2d9fba09ff82df3989877020a541
unknown <luis.almeida.faria@gmail.com>
2025-04-28 17:40:16 -0300

    Initial commit

---
+++ backend/src/main/java/com/project/furia_chat/FuriaChatApplication.java
@@ -0,0 +1,13 @@
+package com.project.furia_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuriaChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/furia_chat/FuriaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.furia_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
