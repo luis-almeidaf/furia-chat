d9b8c12fbe8600fb2c0e7e1e8c8f8f9d5dd98853
unknown <luis.almeida.faria@gmail.com>
2025-05-01 18:17:11 -0300

    chore(db): add migrations for populate database with players

---
+++ backend/src/main/resources/db/migration/V2__Insert_sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO players (nickname, role, img) VALUES
+('Yekindar', 'Rifler', 'https://raw.githubusercontent.com/luis-almeidaf/image-hosting/refs/heads/main/yekindar-.png'),
+('Kscerato', 'Rifler', 'https://raw.githubusercontent.com/luis-almeidaf/image-hosting/refs/heads/main/kscerato-.png'),
+('Molodoy', 'AWPer', 'https://raw.githubusercontent.com/luis-almeidaf/image-hosting/refs/heads/main/molodoy-.png'),
+('yuurih', 'Rifler', 'https://raw.githubusercontent.com/luis-almeidaf/image-hosting/refs/heads/main/yuurih.png'),
+('Fallen', 'IGL - AWPer', 'https://raw.githubusercontent.com/luis-almeidaf/image-hosting/refs/heads/main/fallen.png');
