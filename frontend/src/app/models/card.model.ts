5f1d50b496f43467e64d12238d9c0cd2ec0b734c
unknown <luis.almeida.faria@gmail.com>
2025-05-02 11:30:08 -0300

    feat(frontend): move player model inside models folder and add card model

---
+++ frontend/src/app/models/card.model.ts
@@ -0,0 +1,10 @@
+export interface CardData {
+  title?: string;
+  description?: string;
+  nickname?: string;
+  role?: string;
+  img?: string;
+  opponent?: string;
+  gameDate?: string;
+  result?: string;
+}
