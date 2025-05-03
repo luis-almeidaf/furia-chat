40cc857c393da2b3806ed76c77f9dbf8b5a9baa3
unknown <luis.almeida.faria@gmail.com>
2025-05-03 18:28:09 -0300

    feat(backend): add `CorsConfig`

---
+++ backend/src/main/java/com/project/furia_chat/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.project.furia_chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("https://furia-chat-theta.vercel.app", "http://localhost:4200")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+    }
+}
