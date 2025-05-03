64d8f30ac2f6f89d052cadec462ab4da92296f52
unknown <luis.almeida.faria@gmail.com>
2025-05-03 17:38:35 -0300

    fix(backend): fix production url

--- backend/src/main/java/com/project/furia_chat/config/WebSocketConfig.java
+++ backend/src/main/java/com/project/furia_chat/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/furia-chat-websocket").setAllowedOrigins("furia-chat-theta.vercel.app", "http://localhost:4200").withSockJS();
+        registry.addEndpoint("/furia-chat-websocket").setAllowedOrigins("https://furia-chat-theta.vercel.app", "http://localhost:4200").withSockJS();
     }
 }
