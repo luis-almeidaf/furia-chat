d817a15fcea30044ab58d0a28e5040df98f8d8c1
unknown <luis.almeida.faria@gmail.com>
2025-05-03 22:56:50 -0300

    feat(frontend): add `about-us` page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LineupComponent } from './features/lineup/lineup.component';
+import { AboutUsComponent } from './features/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'lineup', component: LineupComponent },
+  { path: 'about-us', component: AboutUsComponent },
 ];
